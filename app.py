e1125310ade71dfd2784bc0cc2641d5e0c83e7c1
Mahendra <mahendrakatragadda89@gmail.com>
2026-02-11 11:02:20 +0530

    msg

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.divider()
 
 # Patient dashboard
 if role == "Patient":
-    st.subheader("👤 Patients Dashboard")
+    st.subheader("👤 Patient Dashboard")
     st.write("• View your medical reports")
     st.write("• Understand reports in simple language")
     st.write("• Book appointments")
